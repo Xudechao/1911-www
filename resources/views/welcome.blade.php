7f89b0fde42977a46da248e9f686aa51b7d0c09c
Your Name <you@example.com>
2020-07-14 09:27:45 +0000

    PHP

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    PHP
                 </div>
 
                 <div class="links">
