116f6e72060290d33da45938a3dae49cf04a5846
Your Name <you@example.com>
2020-07-15 03:58:07 +0000

    token2

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -14,4 +14,28 @@ class TestController extends Controller
         $cont = file_get_contents($url);
         echo $cont;
     }
+
+    public function getWxToken2()
+    {
+        $appid = 'wx0a0d276f71eea575';
+        $secret = '772b59b747feb34c5788faab6661158d';
+        $url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.$appid.'&secret='.$secret.'';
+
+        // 创建一个新cURL资源
+        $ch = curl_init();
+
+        // 设置URL和相应的选项
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);      // 将返回结果通过变量接收
+
+        // 抓取URL并把它传递给浏览器
+        $response = curl_exec($ch);
+
+        // 关闭cURL资源，并且释放系统资源
+        curl_close($ch);
+
+        echo $response;
+    }
+
 }
