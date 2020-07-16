bc0fff3c826bd0dd6716c254154e0eba08f14273
Your Name <you@example.com>
2020-07-16 01:04:27 +0000

    token5

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -10,7 +10,7 @@ class TestController extends Controller
      public function test1()
      {
          $url = 'http://api.1911.com/user/info';
-         //$url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APPID&secret=APPSECRET'
+         //$url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APPID&secret=APPSECRET';
          $response = file_get_contents($url);
          var_dump($response);
      }
